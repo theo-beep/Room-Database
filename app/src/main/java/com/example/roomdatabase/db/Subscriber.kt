7c8999dbf1f78ce2880b794876615cc8011a7dcf
Theo <theolin@immedia.co.za>
2021-10-29 11:17:33 +0200

    creating an entity class for datbase

--- app/src/main/java/com/example/roomdatabase/db/Subscriber.kt
+++ app/src/main/java/com/example/roomdatabase/db/Subscriber.kt
@@ -1,3 +1,17 @@
 package com.example.roomdatabase.db
 
-data class Subscriber (val id :Int ,val name:String,val email:String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//Entity class for a database
+@Entity(tableName = "subscriber_data_table")
+data class Subscriber(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "subscriber_id")
+    val id: Int,
+    @ColumnInfo(name = "subscriber_name")
+    val name: String,
+    @ColumnInfo(name = "subscriber_email")
+    val email: String
+)
